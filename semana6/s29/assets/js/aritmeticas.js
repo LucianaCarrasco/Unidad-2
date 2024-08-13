68a935239ab6c064555bb3c28662e81d586347e9
Luciana <carrascocarhuasluciana0@gmail.com>
2024-08-12 19:46:41 -0500

    New

---
+++ semana6/s29/assets/js/aritmeticas.js
@@ -0,0 +1,15 @@
+//Usamos la función sumar 
+function sumar(a, b) {
+    return a + b;
+}
+
+//Usamos función restar
+function restar(a, b) {
+    return a - b;
+}
+
+//Exportaciones 
+export {
+    sumar,
+    restar
+};
---
+++ semana6/s29/assets/js/script.js
@@ -0,0 +1,6 @@
+import { sumar, restar } from "./aritmeticas.js"
+
+//Usamos la función sumar
+console.log(sumar(12, 16));
+//Usamos la función restar
+console.log(restar(30, 13));
